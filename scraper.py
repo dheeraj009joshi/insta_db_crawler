600d276a2cbf836c675aaa2995da9d2bfd456148
Dheeraj Joshi <dlovej009@gmail.com>
2025-08-05 09:27:40 +0530

    dj

--- scraper.py
+++ scraper.py
@@ -25,7 +25,8 @@ class Scraper:
     def scrape_post_comments(self, post_id, count):
         print(f"[+] Scraping comments for post ID: {post_id}")
         try:
-            comments = self.cl.media_comments(id=post_id, count=count)
+            comments = self.cl.media_comments(id=str(post_id), count=count)
+            print(comments)
             return [item.get("text", "") for item in comments]
         except Exception as e:
             print(f"[!] Error fetching comments: {e}")
--- test.py
+++ test.py
@@ -1,44 +1,51 @@
-import csv
+# import csv
 
-from db import collection,collection2
+# from db import collection,collection2
 
-# === Open CSV file for writing ===
-with open("output.csv", "w", newline='', encoding='utf-8') as csvfile:
-    fieldnames = ["post_id", "description", "videoUrl", "username", "createTime", "playCount","transcript", "comment_count", "comments"]
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-    writer.writeheader()
+# # === Open CSV file for writing ===
+# with open("output.csv", "w", newline='', encoding='utf-8') as csvfile:
+#     fieldnames = ["post_id", "description", "videoUrl", "username", "createTime", "playCount","transcript", "comment_count", "comments"]
+#     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+#     writer.writeheader()
 
-    # === Iterate through documents ===
-    for doc in collection.find({}):
-        row = {
-            "post_id": doc.get("post_id", {}),
-            "description": doc.get("description", ""),
-            "videoUrl": doc.get("videoUrl", ""),
-            "username": doc.get("user_info", {}),
-            "createTime": doc.get("createTime", ""),
-            "playCount": doc.get("playCount", {}),
-            "transcript": doc.get("transcript", {}),
-            "comment_count": doc.get("comment_count", {}),
-            "comments": " | ".join([c.strip() for c in doc.get("comments", []) if c.strip()])
-        }
-        writer.writerow(row)
-    for doc in collection2.find({}):
-        row = {
-            "post_id": doc.get("post_id", {}),
-            "description": doc.get("description", ""),
-            "videoUrl": doc.get("videoUrl", ""),
-            "username": doc.get("user_info", {}),
-            "createTime": doc.get("createTime", ""),
-            "playCount": doc.get("playCount", {}),
-             "transcript": doc.get("transcript", {}),
-            "comment_count": doc.get("comment_count", {}),
-            "comments": " | ".join([c.strip() for c in doc.get("comments", []) if c.strip()])
-        }
-        writer.writerow(row)
+#     # === Iterate through documents ===
+#     for doc in collection.find({}):
+#         row = {
+#             "post_id": doc.get("post_id", {}),
+#             "description": doc.get("description", ""),
+#             "videoUrl": doc.get("videoUrl", ""),
+#             "username": doc.get("user_info", {}),
+#             "createTime": doc.get("createTime", ""),
+#             "playCount": doc.get("playCount", {}),
+#             "transcript": doc.get("transcript", {}),
+#             "comment_count": doc.get("comment_count", {}),
+#             "comments": " | ".join([c.strip() for c in doc.get("comments", []) if c.strip()])
+#         }
+#         writer.writerow(row)
+#     for doc in collection2.find({}):
+#         row = {
+#             "post_id": doc.get("post_id", {}),
+#             "description": doc.get("description", ""),
+#             "videoUrl": doc.get("videoUrl", ""),
+#             "username": doc.get("user_info", {}),
+#             "createTime": doc.get("createTime", ""),
+#             "playCount": doc.get("playCount", {}),
+#              "transcript": doc.get("transcript", {}),
+#             "comment_count": doc.get("comment_count", {}),
+#             "comments": " | ".join([c.strip() for c in doc.get("comments", []) if c.strip()])
+#         }
+#         writer.writerow(row)
 
-print("✅ Exported to output2.csv")
+# print("✅ Exported to output2.csv")
 
+from scraper import Scraper
 
+aa=Scraper(token="0daja8wqtv3o16jpszpj582tbyduul3t")
 
+comments=aa.scrape_post_comments(
+    post_id="3691571466180023792",
+    count=1000
+)
 
+print(comments)
 
